33493dcd9168e9f8f81b9240eb49fbdc14e33194
USER <user@USER-no-MacBook-Pro.local>
2019-07-17 15:20:18 +0900

    Finish Bookers

---
+++ db/migrate/20190717060408_books.rb
@@ -0,0 +1,5 @@
+class Books < ActiveRecord::Migration[5.2]
+  def change
+  	drop_table :books
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_110117) do
+ActiveRecord::Schema.define(version: 2019_07_17_060513) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
-    t.string "body"
   end
 
 end
